36e714378476788a69d6ad697cf41f2de1938bf8
eljam3239 <elijames2301@gmail.com>
2022-07-05 23:39:23 -0400

    Brainstorm simple interface and use case

---
+++ interface.py
@@ -0,0 +1,4 @@
+#sql database?
+
+#GIVEN a database of "mdeical records", allow a file to be pulled if specific password is entered.
+#not only correct password, but password for the appropriate username
