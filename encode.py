160ecf0043e79f2e49f3f74fd5076b445ffb8487
eljam3239 <elijames2301@gmail.com>
2022-07-05 23:34:28 -0400

    Write encrypt and decrypt functions. Test this usign a txt file or somehting if you want.

--- encode.py
+++ encode.py
@@ -1,4 +1,5 @@
 from cryptography.fernet import Fernet
+from rsa import encrypt
 
 def write_key():
     key = Fernet.generate_key()
@@ -8,5 +9,35 @@ def write_key():
 def load_key():
     return open("key.key", "rb").read()
 
-write_key()
+#write_key()
+key = load_key()
 
+def encrypt(filename, key):
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        file_data = file.read()
+        encrypted_data = f.encrypt(file_data)
+    with open(filename, "wb") as file:
+        file.write(encrypted_data)
+def decrypt(filename, key):
+    f=Fernet(key)
+    with open(filename, "rb") as file:
+        ecrypted_data = file.read()
+
+    decrypted_data = f.decrypt(ecrypted_data)
+    with open(filename, "wb") as file:
+        file.write(decrypted_data)
+"""
+Test encryption and decryption of a string
+test = "blah blah blah".encode()
+f = Fernet(key)
+encrypted = f.encrypt(test)
+print(encrypted)
+decrypted_encrypted = f.decrypt(encrypted)
+print(decrypted_encrypted)
+
+"""
+file = "test.txt"
+#encrypt(file, key)
+
+decrypt(file, key)
